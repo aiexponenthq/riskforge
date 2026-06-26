1161a95829630641560da0f6dbd48d67f19b72df
Ajay Pundhir <ajaypratap.iiitb@gmail.com>
2026-06-26 18:56:23 +0400

    chore: Clean up secondary test files

--- patch.py
+++
@@ -1,8 +0,0 @@
-with open("src/riskforge/storage/filesystem.py", "r") as f:
-    content = f.read()
-
-content = content.replace("        self._audit_lock = asyncio.Lock()\n", "        self._async_lock = None\n")
-content = content.replace("self._async_lock is None:", "getattr(self, '_async_lock', None) is None:")
-
-with open("src/riskforge/storage/filesystem.py", "w") as f:
-    f.write(content)
